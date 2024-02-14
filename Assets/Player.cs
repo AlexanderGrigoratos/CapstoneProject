7b23c4edb5ea0fca37b592806bd5aa7ed9b81849
Alex <alexandergrigoratos@hotmail.com>
2024-02-14 15:48:57 -0500

    Experimenting with movement
    Using debug scripts in order to project movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float xInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start was called.");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            xInput = Input.GetAxisRaw("Horizontal");
+            Debug.Log("Jump");
+        }
     }
 }
