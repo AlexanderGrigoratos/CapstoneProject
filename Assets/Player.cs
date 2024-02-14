c164b1567543bbb951534fe2d52aa1f5ccf39ae3
Alex <alexandergrigoratos@hotmail.com>
2024-02-14 16:15:23 -0500

    Code clean up

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float movespeed;
-    public float jumpforce;
+    private Rigidbody2D rb;
+
+    [SerializeField] private float movespeed;
+    [SerializeField] private float jumpforce;
+   
+    
     private float xInput;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
        
     }
 
@@ -18,8 +24,8 @@ public class Player : MonoBehaviour
     void Update()
     {
        
-            xInput = Input.GetAxisRaw("Horizontal");
-            rb.velocity = new Vector2 (xInput * movespeed, rb.velocity.y);
+        xInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2 (xInput * movespeed, rb.velocity.y);
           
         if (Input.GetKeyDown(KeyCode.Space))
         {
