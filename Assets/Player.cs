b09b5a0c7e11b5c418b48fee343778eeb1ee5ee1
Alex <alexandergrigoratos@hotmail.com>
2024-02-15 21:55:02 -0500

    Code Cleanup

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -24,19 +24,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
+        Movement();
+        CheckInput();
+
+        AnimatorControllers();
+
+    }
+
+    private void CheckInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2 (xInput * movespeed, rb.velocity.y);
-          
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+            Jump();
         }
+    }
 
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * movespeed, rb.velocity.y);
+    }
 
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+    }
+
+    private void AnimatorControllers()
+    {
         isMoving = rb.velocity.x != 0;
 
         anim.SetBool("isMoving", isMoving);
-       
+
     }
 }
