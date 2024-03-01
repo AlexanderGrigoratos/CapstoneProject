99c6fcb2b96fa520da6146df1d88f69d7c5b4215
Alex <alexandergrigoratos@hotmail.com>
2024-02-29 23:17:57 -0500

    State Machine creation
    Attempting the idea of a state machine, possible redo of code needed from previous entries

---
+++ Assets/PlayerState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    protected PlayerStateMachine stateMachine;
+    protected Player player;
+
+    private string animBoolName;
+
+    public PlayerState(Player _player, PlayerStateMachine stateMachine, string _animBoolName)
+    {
+        this.player = _player;
+        this.stateMachine = stateMachine;
+        this.animBoolName = _animBoolName;
+    }
+
+    public virtual void Enter()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public virtual void Exit()
+    {
+
+    }
+
+
+    
+}
