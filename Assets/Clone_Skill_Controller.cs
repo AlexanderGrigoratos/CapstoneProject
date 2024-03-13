c6df036abf1fbac1244f951f3397003ad762810a
Alex <alexandergrigoratos@hotmail.com>
2024-03-13 16:36:42 -0400

    Clone attack

--- Assets/Clone_Skill_Controller.cs
+++ Assets/Clone_Skill_Controller.cs
@@ -7,13 +7,17 @@ public class Clone_Skill_Controller : MonoBehaviour
 {
 
     private SpriteRenderer sr;
+    private Animator anim;
     [SerializeField] private float colorLosingSpeed;
 
     private float cloneTimer;
+    [SerializeField] private Transform attackCheck;
+    [SerializeField] private float attackCheckRadius = .8f;
 
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -25,11 +29,28 @@ public class Clone_Skill_Controller : MonoBehaviour
             sr.color = new Color(1, 1, 1, sr.color.a - Time.deltaTime * colorLosingSpeed);
         }
     }
-    public void SetupClone(Transform _newTransform, float _cloneDuration)
+    public void SetupClone(Transform _newTransform, float _cloneDuration, bool _canAttack)
     {
+        if (_canAttack)
+            anim.SetInteger("AttackNumber", Random.Range(1, 3));
+
         transform.position = _newTransform.position;
         cloneTimer = _cloneDuration;
     }
+    private void AnimationTrigger()
+    {
+        cloneTimer = -.1f;
+    }
 
+    private void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(attackCheck.position, attackCheckRadius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+                hit.GetComponent<Enemy>().Damage();
+        }
+    }
 
 }
