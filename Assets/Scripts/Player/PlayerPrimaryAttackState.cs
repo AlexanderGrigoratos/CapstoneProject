a822f2bf5e9ebffe758af99c992997810eb0d870
Alex <alexandergrigoratos@hotmail.com>
2024-03-13 13:46:39 -0400

    bug hotfix

--- Assets/Scripts/Player/PlayerPrimaryAttackState.cs
+++ Assets/Scripts/Player/PlayerPrimaryAttackState.cs
@@ -18,6 +18,8 @@ public class PlayerPrimaryAttackState : PlayerState
     {
         base.Enter();
 
+        xInput = 0;
+
         if (comboCounter > 2 || Time.time >= lastTimeAttacked + comboWindow)
             comboCounter = 0;
 
