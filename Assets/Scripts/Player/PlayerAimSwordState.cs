b53fd5a0cde7850429db7817cf11bf43b5c02da0
Alex <alexandergrigoratos@hotmail.com>
2024-03-14 20:28:49 -0400

    sword throw skill
    almost working, a few bugs

--- Assets/Scripts/Player/PlayerAimSwordState.cs
+++ Assets/Scripts/Player/PlayerAimSwordState.cs
@@ -12,6 +12,8 @@ public class PlayerAimSwordState : PlayerState
     public override void Enter()
     {
         base.Enter();
+
+        player.skill.sword.DotsActive(true);
     }
 
     public override void Exit()
