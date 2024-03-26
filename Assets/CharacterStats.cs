055bf282fc3e6315df7296afc24f2afe6be8a09f
Alex <alexandergrigoratos@hotmail.com>
2024-03-26 19:39:40 -0400

    Defensive stats

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -1,14 +1,27 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    [Header("Major Stats")]
+    public Stat strength; //1 point increase by 1 and crit by 1%
+    public Stat agility; // increase vision by 1% and crit by 1%
+    public Stat intelligence; //increase magic damage by 1 and magic resistance by 3
+    public Stat vitality; // increase health by 3 or 5
 
-    public Stat Strength;
+    [Header("Offensive Stats")]
     public Stat damage;
+    public Stat critChance;
+    public Stat critPower;
+
+    [Header("Defensive Stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+
+
+
+
+
 
    
 
@@ -21,13 +34,15 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamage(CharacterStats _targetStats)
     {
+        if (TargetCanAvoidAttack(_targetStats))
+            return;
 
-        
+        int totalDamage = damage.GetValue() + strength.GetValue();
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
 
-          
-        int totalDamage = damage.GetValue() + Strength.GetValue();
         _targetStats.TakeDamage(totalDamage);
     }
+
 
     public virtual void TakeDamage(int _damage)
     {
@@ -44,5 +59,23 @@ public class CharacterStats : MonoBehaviour
     protected virtual void Die()
     {
         
+    }
+    private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage -= _targetStats.armor.GetValue();
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        return totalDamage;
+    }
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            return true;
+
+        }
+
+        return false;
     }
 }
