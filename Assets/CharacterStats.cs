ed61bf1bdb08928bf708566b418eb276ed551f54
Alex <alexandergrigoratos@hotmail.com>
2024-03-27 13:32:44 -0400

    applying ailments

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -69,8 +69,49 @@ public class CharacterStats : MonoBehaviour
         int _lightningDamage = lightningDamage.GetValue();
 
         int totalMagicalDamage = _fireDamage + _iceDamage + _lightningDamage + intelligence.GetValue();
+
         totalMagicalDamage = CheckTargetResistance(_targetStats, totalMagicalDamage);
         _targetStats.TakeDamage(totalMagicalDamage);
+
+
+        if (Mathf.Max(_fireDamage, _iceDamage, _lightningDamage) <= 0)
+            return;
+
+        bool canApplyIgnite = _fireDamage > _iceDamage && _fireDamage > _lightningDamage;
+        bool canApplyChill = _iceDamage > _fireDamage && _iceDamage > _lightningDamage;
+        bool canApplyShock = _lightningDamage > _fireDamage && _lightningDamage > _iceDamage;
+
+        while(!canApplyIgnite && !canApplyChill && !canApplyShock)
+        {
+            if(Random.value < .3f && _fireDamage > 0)
+            {
+                canApplyIgnite = true;
+                _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+                Debug.Log("applied fire");
+                return;
+            }
+
+            if (Random.value < .5f && _iceDamage > 0)
+            {
+                canApplyChill = true;
+                _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+                Debug.Log("applied ice");
+                return;
+            }
+
+            if (Random.value < .5f && _lightningDamage > 0)
+            {
+                canApplyShock = true;
+                Debug.Log("applied lightning");
+                _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+                return;
+            }
+        }
+
+
+        _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+
+
     }
 
     private static int CheckTargetResistance(CharacterStats _targetStats, int totalMagicalDamage)
