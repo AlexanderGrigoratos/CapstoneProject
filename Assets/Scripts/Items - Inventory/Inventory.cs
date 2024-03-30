b98591f6c72c08cbc7f28ebc0dbf5a2eaf427e5b
Alex <alexandergrigoratos@hotmail.com>
2024-03-30 13:53:51 -0400

    Item slots and beginning adding modifiers to items

--- Assets/Scripts/Items - Inventory/Inventory.cs
+++ Assets/Scripts/Items - Inventory/Inventory.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,19 +5,24 @@ public class Inventory : MonoBehaviour
 {
     public static Inventory instance;
 
+    public List<InventoryItem> equipment;
+    public Dictionary<ItemData_Equipment, InventoryItem> equipmentDictionary;
+
     public List<InventoryItem> inventory;
     public Dictionary<ItemData, InventoryItem> inventoryDictionary;
 
     public List<InventoryItem> stash;
-    public Dictionary <ItemData, InventoryItem> stashDictionary;
+    public Dictionary<ItemData, InventoryItem> stashDictionary;
 
     [Header("Inventory UI")]
 
     [SerializeField] private Transform inventorySlotParent;
     [SerializeField] private Transform stashSlotParent;
+    [SerializeField] private Transform equipmentSlotParent;
 
     private UI_ItemSlot[] inventoryItemSlot;
     private UI_ItemSlot[] stashItemSlot;
+    private UI_EquipmentSlot[] equipmentSlot;
 
     private void Awake()
     {
@@ -35,12 +39,75 @@ public class Inventory : MonoBehaviour
         stash = new List<InventoryItem>();
         stashDictionary = new Dictionary<ItemData, InventoryItem>();
 
+        equipment = new List<InventoryItem>();
+        equipmentDictionary = new Dictionary<ItemData_Equipment, InventoryItem>();
+
         inventoryItemSlot = inventorySlotParent.GetComponentsInChildren<UI_ItemSlot>();
         stashItemSlot = stashSlotParent.GetComponentsInChildren<UI_ItemSlot>();
+        equipmentSlot = equipmentSlotParent.GetComponentsInChildren<UI_EquipmentSlot>();
+    }
+
+    public void EquipItem(ItemData _item)
+    {
+        ItemData_Equipment newEquipment = _item as ItemData_Equipment;
+        if (newEquipment == null) return;
+
+        InventoryItem newItem = new InventoryItem(newEquipment);
+        ItemData_Equipment oldEquipment = null;
+
+        foreach (KeyValuePair<ItemData_Equipment, InventoryItem> item in equipmentDictionary)
+        {
+            if (item.Key.equipmentType == newEquipment.equipmentType)
+                oldEquipment = item.Key;
+        }
+
+        if (oldEquipment != null)
+        {
+            UnequipItem(oldEquipment);
+            AddItem(oldEquipment);
+
+        }
+
+        equipment.Add(newItem);
+        equipmentDictionary.Add(newEquipment, newItem);
+        RemoveItem(_item);
+
+        UpdateSlotUI();
+    }
+
+    private void UnequipItem(ItemData_Equipment itemToRemove)
+    {
+        if (itemToRemove == null) return;
+
+        if (equipmentDictionary.TryGetValue(itemToRemove, out InventoryItem value))
+        {
+            equipment.Remove(value);
+            equipmentDictionary.Remove(itemToRemove);
+        }
     }
 
     private void UpdateSlotUI()
     {
+        for (int i = 0; i < equipmentSlot.Length; i++)
+        {
+            foreach (KeyValuePair<ItemData_Equipment, InventoryItem> item in equipmentDictionary)
+            {
+                if (item.Key.equipmentType == equipmentSlot[i].slotType)
+                    equipmentSlot[i].UpdateSlot(item.Value);
+            }
+        }
+
+        for (int i = 0; i < inventoryItemSlot.Length; i++)
+        {
+            inventoryItemSlot[i].CleanUpSlot();
+        }
+
+        for (int i = 0; i < stashItemSlot.Length; i++)
+        {
+            stashItemSlot[i].CleanUpSlot();
+        }
+
+
         for (int i = 0; i < inventory.Count; i++)
         {
             inventoryItemSlot[i].UpdateSlot(inventory[i]);
@@ -57,10 +124,10 @@ public class Inventory : MonoBehaviour
         if (_item.itemType == ItemType.Equipment)
             AddToInventory(_item);
 
-        
+
         else if (_item.itemType == ItemType.Material)
             AddToStash(_item);
-        
+
 
         UpdateSlotUI();
     }
@@ -107,7 +174,7 @@ public class Inventory : MonoBehaviour
 
         if (stashDictionary.TryGetValue(_item, out InventoryItem stashValue))
         {
-            if(stashValue.stackSize <= 1)
+            if (stashValue.stackSize <= 1)
             {
                 stash.Remove(stashValue);
                 stashDictionary.Remove(_item);
@@ -116,7 +183,7 @@ public class Inventory : MonoBehaviour
                 stashValue.RemoveStack();
         }
 
-        UpdateSlotUI() ;
+        UpdateSlotUI();
     }
 
 
--- Assets/Scripts/Items - Inventory/ItemData_Equipment.cs
+++ Assets/Scripts/Items - Inventory/ItemData_Equipment.cs
@@ -12,8 +12,56 @@ public enum EquipmentType
 
 public class ItemData_Equipment : ItemData
 {
-   public EquipmentType equipmenttype;
+   public EquipmentType equipmentType;
 
+    [Header("Major Stats")]
+    public int strength;
+    public int agility;
+    public int intelligence;
+    public int vitality;
 
+    [Header("Offensive Stats")]
+    public int damage;
+    public int critChance;
+    public int critPower;
+
+    [Header("Defensive Stats")]
+    public int maxHealth;
+    public int armor;
+    public int evasion;
+    public int magicResistance;
+
+    [Header("Magic Stats")]
+    public int fireDamage;
+    public int iceDamage;
+    public int lightningDamage;
+
+    public void AddModifiers()
+    {
+        PlayerStats playerstats = PlayerManager.instance.player.GetComponent<PlayerStats>();
+
+        playerstats.strength.AddModifier(strength);
+        playerstats.agility.AddModifier(agility);
+        playerstats.intelligence.AddModifier(intelligence);
+        playerstats.vitality.AddModifier(vitality);
+
+        playerstats.damage.AddModifier(damage);
+        playerstats.critChance.AddModifier(critChance);
+        playerstats.critPower.AddModifier(critPower);
+
+        playerstats.maxHealth.AddModifier(maxHealth);
+        playerstats.armor.AddModifier(armor);
+        playerstats.evasion.AddModifier(evasion);
+        playerstats.magicResistance.AddModifier(magicResistance);
+
+        playerstats.fireDamage.AddModifier(fireDamage);
+        playerstats.iceDamage.AddModifier(iceDamage);
+        playerstats.lightningDamage.AddModifier(lightningDamage);
+    }
+
+    public void RemoveModifiers()
+    {
+
+    }
 
 }
