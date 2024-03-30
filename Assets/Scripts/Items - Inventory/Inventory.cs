4b2e5cec78eb33ac1b9aed8a7171389698fe6d91
Alex <alexandergrigoratos@hotmail.com>
2024-03-30 16:53:28 -0400

    crafting system

--- Assets/Scripts/Items - Inventory/Inventory.cs
+++ Assets/Scripts/Items - Inventory/Inventory.cs
@@ -189,5 +189,41 @@ public class Inventory : MonoBehaviour
         UpdateSlotUI();
     }
 
+    public bool CanCraft(ItemData_Equipment _itemToCraft, List<InventoryItem> _requiredMaterials)
+    {
+        List<InventoryItem> materialsToRemove = new List<InventoryItem>();
+
+        for (int i= 0; i<_requiredMaterials.Count; i++)
+        {
+            if (stashDictionary.TryGetValue(_requiredMaterials[i].data, out InventoryItem stashValue))
+            {
+                if (stashValue.stackSize < _requiredMaterials[i].stackSize)
+                {
+                    Debug.Log("Not Enough Materials!");
+                    return false;
+                }
+                else
+                {
+                    materialsToRemove.Add(stashValue);
+                }
+            }
+            else
+            {
+                Debug.Log("Not Enough Materials!");
+                return false;
+            }
+        }
+
+        for (int i = 0; i < materialsToRemove.Count; i++)
+        {
+            RemoveItem(materialsToRemove[i].data);
+        }
+
+        AddItem(_itemToCraft);
+        Debug.Log("Here is your item " + _itemToCraft.name);
+
+        return true;
+    }
+
 
 }
